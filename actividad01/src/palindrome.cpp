f90b705e2da86b99ad975e8966a26d7c5064ca5a
danielmgd <79357841+danielmgd@users.noreply.github.com>
2021-02-20 15:14:29 +0100

    palindrome.ccp
    actividad01

--- actividad01/src/palindrome.cpp
+++ actividad01/src/palindrome.cpp
@@ -1,6 +1,42 @@
 
-#include "palindrome.h"
+#include <iostream>
+#include <vector>
 
-bool is_palindrome(std::string word) {
-    return false;
+using namespace std;
+
+
+    bool is_palindrome(string word) {
+        int cont=0;
+        int a=word.size();
+        for (int i=0; i<a;i++){
+            if(word[i]==' '){
+                 cont++;
+                for(int j=i+1; j<=a;j++){
+                    word[j-1]=word[j];
+                }
+            }
+        }
+        for(int i=0; i<cont;i++){
+            word.pop_back();
+        }
+
+        string resultante;
+        for(int i=word.size()-1;i>=0;i--){
+            resultante+= word[i];
+        }
+        if(word==resultante){
+            return true ;
+        }else{
+            return false ;
+        }
+    }
+    int main() {
+        std::vector<std::string> phrases{{"ana", "yo soy", "isaac no ronca asi"}};
+
+        for ( auto word: phrases) {
+             std::cout << word << "? " << is_palindrome(word) << std::endl;
+        }
+
+
+    return 0;
 }
